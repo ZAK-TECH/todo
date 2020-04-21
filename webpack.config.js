3e2153fa32cedf113fb39c9a57baadc1afa6e727
Abderrezak FENOUCHE <abderrezak@swissdidata.com>
2020-04-21 19:02:58 +0100

    Added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,19 @@ module.exports={
         path: path.resolve(__dirname,'dist'),
         filename:'js/[name].js'
  
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 }
