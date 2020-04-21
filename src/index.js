a0a65ad9c53a00b236c516cd6b5a74d715f26447
Abderrezak FENOUCHE <abderrezak@swissdidata.com>
2020-04-22 00:23:55 +0100

    Added setDone and setToDo functions to change the state of Todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import {RenderList}from './modules/RenderList';
 import {AddTodo} from './modules/Todo';
+import {setToDo} from './modules/Todo';
+import {setDone} from './modules/Todo';
 
 
 RenderList();
 AddTodo();
+setToDo();
+setDone();
--- src/modules/RenderList.js
+++ src/modules/RenderList.js
@@ -8,27 +8,27 @@
 
     if(lastList!=null)todolist=lastList;
     todolist.forEach( (todo)=>{
-
+  
     let row = table.insertRow(0);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
     let cell5=row.insertCell(4);
-
-    
+ 
     cell1.innerHTML = todo.name;
     cell2.innerHTML=todo.toDoDate;
     if(todo.state=="ToDo"){
         cell3.innerHTML=`<span class="badge badge-warning">${todo.state}</span>`;
-        cell4.innerHTML=`<div class="dropdown"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">State</button><div class="dropdown-menu"><button class="dropdown-item" onclick="setDone(${todo.id});return true">Done</button></div></div>`;    }
+        cell4.innerHTML=`<div class="dropdown"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">State</button><div class="dropdown-menu"><button class="dropdown-item" id="D${todo.id}">Done</button></div></div>`;    }
     else {
         cell3.innerHTML=`<span class="badge  badge-success">${todo.state}</span>`;
-        cell4.innerHTML=`<div class="dropdown"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">State</button><div class="dropdown-menu"><button class="dropdown-item btn btn-primary "  onclick="setToDo(${todo.id});return true" >ToDo</button></div></div>`;
+        cell4.innerHTML=`<div class="dropdown"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">State</button><div class="dropdown-menu"><button class="dropdown-item btn btn-primary "  id="T${todo.id}" >ToDo</button></div></div>`;
     }
   
     cell5.innerHTML=`<div class="custom-control custom-switch"><input type="checkbox" class="custom-control-input" id="customSwitch${todo.id}" ><label class="custom-control-label" for="customSwitch${todo.id}"></label></div>`;
 
+   
     });
 
   }
